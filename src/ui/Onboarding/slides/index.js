b4bdffb30131ca89d3136623a7824323df25f483
Caleb Panza <caleb.panza@icloud.org>
2019-07-22 13:53:21 -0400

    Onboarding Profile Information
    Created a page for onboarding that asks for First Name, Last Name, Gender, and Birthdate. It then updates the Person record upon submitting. First Name and Last Name are required, while the others are optional

--- src/ui/Onboarding/slides/index.js
+++ src/ui/Onboarding/slides/index.js
@@ -1,3 +1,4 @@
 export { default as LandingPage } from './LandingPage'
 export { default as Passcode } from './Passcode'
 export { default as ProfileInformation } from './ProfileInformation'
+export { default as EnableNotifications } from './EnableNotifications'
