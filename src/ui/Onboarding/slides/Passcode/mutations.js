d41a344772b1c5309a81af3ea00571ee84eb9c8d
Caleb Panza <caleb.panza@icloud.org>
2019-07-26 12:14:09 -0400

    Passcode onboarding screen

--- src/ui/Onboarding/slides/Passcode/mutations.js
+++ src/ui/Onboarding/slides/Passcode/mutations.js
@@ -1,23 +1,28 @@
 import gql from 'graphql-tag';
 
-export const VERIFY_PIN = gql`
-  mutation verifyPin($username: String!, $password: String!) {
-    authenticateWithSms(phoneNumber: $username, pin: $password) {
+export const AUTHENTICATE_CREDENTIALS = gql`
+  mutation authenticateCredentials($username: String!, $password: String!) {
+    authenticateCredentials(identity: $username, passcode: $password) {
       token
     }
   }
-`;
-
-export const VERIFY_PASSWORD = gql`
-  mutation authenticate($username: String!, $password: String!) {
-    authenticate(identity: $username, password: $password) {
-      token
-    }
-  }
-`;
-
+`
 export const HANDLE_LOGIN = gql`
   mutation handleLogin($authToken: String!) {
     handleLogin(authToken: $authToken) @client
   }
-`;
+`
+export const CREATE_NEW_LOGIN = gql`
+  mutation createNewUserAccount($identity: String!, $passcode: String!) {
+    createNewUserLogin(identity: $identity, passcode: $passcode) {
+      token
+    }
+  }
+`
+export const HANDLE_NEW_LOGIN = gql`
+  mutation handleNewUserAccount($identity: String!, $passcode: String!) {
+    createNewUserAccount(identity: $identity, passcode: $passcode) {
+      token
+    }
+  }
+`
