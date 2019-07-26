5bb9ac2239dc495d170da2fe9e01361ad02f1c7c
Caleb Panza <caleb.panza@icloud.org>
2019-07-26 12:15:15 -0400

    updated onboarding slide references

--- src/ui/Onboarding/slides/index.js
+++ src/ui/Onboarding/slides/index.js
@@ -1,4 +1,4 @@
-export { default as LandingPage } from './LandingPage'
+export { default as Identity } from './Identity'
 export { default as Passcode } from './Passcode'
 export { default as ProfileInformation } from './ProfileInformation'
 export { default as EnableNotifications } from './EnableNotifications'
