6b74c1b632359dadd97247ac0d08eb83167134ad
Caleb Panza <caleb.panza@icloud.org>
2019-07-26 12:15:29 -0400

    updated Onboarding stack navigator

--- src/ui/Onboarding/index.js
+++ src/ui/Onboarding/index.js
@@ -1,71 +1,27 @@
 import React from 'react';
 
-import { GradientOverlayImage } from '@apollosproject/ui-kit';
-import { ApolloConsumer } from 'react-apollo';
-
 import {
-  AskNotificationsConnected,
-  AskNameConnected,
-  FeaturesConnected,
-  AboutYouConnected,
-  LocationFinderConnected,
-  OnboardingSwiper,
-} from '@apollosproject/ui-onboarding';
-
-import { requestPushPermissions } from '@apollosproject/ui-notifications';
-
-import {
-  LandingPage, PassCode, ProfileInformation, EnableNotifications
+  Identity, Passcode, ProfileInformation, EnableNotifications
 } from './slides'
 
-function Onboarding({ navigation }) {
-  return (
-    <OnboardingSwiper>
-      {({ swipeForward }) => (
-        <>
-          <AskNameConnected onPressPrimary={swipeForward} />
-          <FeaturesConnected
-            onPressPrimary={swipeForward}
-            BackgroundComponent={
-              <GradientOverlayImage
-                source={'https://picsum.photos/640/640/?random'}
-              />
-            }
-          />
-          <AboutYouConnected
-            onPressPrimary={swipeForward}
-            BackgroundComponent={
-              <GradientOverlayImage
-                source={'https://picsum.photos/640/640/?random'}
-              />
-            }
-          />
-          <ApolloConsumer>
-            {(client) => (
-              <AskNotificationsConnected
-                onPressPrimary={() => navigation.replace('Tabs')}
-                onRequestPushPermissions={() =>
-                  requestPushPermissions({ client })
-                }
-                primaryNavText={'Finish'}
-                BackgroundComponent={
-                  <GradientOverlayImage
-                    source={'https://picsum.photos/640/640/?random'}
-                  />
-                }
-              />
-            )}
-          </ApolloConsumer>
-        </>
-      )}
-    </OnboardingSwiper>
-  );
-}
+import { createStackNavigator } from 'react-navigation';
 
-Onboarding.navigationOptions = {
-  title: 'Onboarding',
+const OnboardingNavigator = createStackNavigator(
+  {
+    Identity,
+    Passcode,
+    ProfileInformation,
+    EnableNotifications,
+  },
+  {
+    initialRouteName: 'Identity',
+  }
+);
+
+OnboardingNavigator.navigationOptions = {
   header: null,
-  gesturesEnabled: false,
 };
 
-export default Onboarding;
+const Onboarding = (props) => <OnboardingNavigator {...props} />
+
+export default OnboardingNavigator
