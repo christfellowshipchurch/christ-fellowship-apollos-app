6472617a9ada6171ab812396546d25b7a9fe6f50
Caleb Panza <caleb.panza@icloud.org>
2019-11-21 09:26:55 -0500

    Updated styling for TableViews per design

--- src/tabs/more/More.js
+++ src/tabs/more/More.js
@@ -1,22 +1,16 @@
 import React from 'react'
-import { SafeAreaView, ScrollView } from 'react-native'
+import { ScrollView } from 'react-native'
+import SafeAreaView from 'react-native-safe-area-view'
 import PropTypes from 'prop-types'
 import {
   H4,
   H3,
   styled,
   BackgroundView,
-  FlexedView
-} from '@apollosproject/ui-kit';
-import { 
-  faUsers,
-  faHandshake,
-  faCalendarAlt,
-  faEnvelopeOpenDollar,
-  faSearch,
-} from '@fortawesome/pro-light-svg-icons'
+  FlexedView,
+  withTheme
+} from '@apollosproject/ui-kit'
 
-import Events from './events'
 import {
   TableView, Cell
 } from 'ChristFellowship/src/ui/TableView'
@@ -24,67 +18,56 @@ import {
 const HeaderTitle = styled(({ theme, active }) => ({
   fontWeight: 'bold',
   textAlign: 'center',
+  paddingVertical: theme.sizing.baseUnit
 }))(H3)
 
-const HeaderSubTitle = styled(({ theme, active }) => ({
-  fontWeight: 'bold',
-  fontSize: 18,
-  padding: 20
-}))(H4)
-
 const More = ({ navigation }) => (
-        <ScrollView>
+  <BackgroundView>
+    <SafeAreaView forceInset={{ bottom: 'never' }}>
+      <HeaderTitle>
+        More
+      </HeaderTitle>
+      <ScrollView>
+        <TableView title='Get Involved'>
+          <Cell
+            icon='handshake'
+            title='Groups'
+            onPress={() => navigation.navigate('Events')}
+          />
+          <Cell
+            icon='users'
+            title='Serve'
+            onPress={() => navigation.navigate('Events')}
+          />
+          <Cell
+            icon='calendar-alt'
+            title='Events'
+            onPress={() => navigation.navigate('Events')}
+          />
+          <Cell
+            icon='envelope-open-dollar'
+            title='Give'
+            onPress={() => navigation.navigate('Events')}
+          />
+        </TableView>
 
-          <HeaderTitle>
-            More
-          </HeaderTitle>
-
-          <HeaderSubTitle>
-            Get Involved
-          </HeaderSubTitle>
-
-          <TableView>
-            <Cell
-              icon={faCalendarAlt}
-              title='Groups'
-              onPress={() => navigation.navigate('Events')}
-            />
-            <Cell
-              icon={faUsers}
-              title='Serve'
-              onPress={() => navigation.navigate('Events')}
-            />
-            <Cell
-              icon={faHandshake}
-              title='Events'
-              onPress={() => navigation.navigate('Events')}
-            />
-            <Cell
-              icon={faEnvelopeOpenDollar}
-              title='Give'
-              onPress={() => navigation.navigate('Events')}
-            />
-          </TableView>
-
-          <HeaderSubTitle>
-            Our Church
-        </HeaderSubTitle>
-
-          <TableView>
-            <Cell
-              title='About Christ Fellowship'
-              onPress={() => navigation.navigate('Events')}
-            />
-            <Cell
-              title='Church Locations'
-              onPress={() => navigation.navigate('Events')}
-            />
-            <Cell
-              title='Contact'
-              onPress={() => navigation.navigate('Events')}
-            />
-          </TableView>
-        </ScrollView>
+        <TableView title='Our Church' padded>
+          <Cell
+            title='About Christ Fellowship'
+            onPress={() => navigation.navigate('Events')}
+          />
+          <Cell
+            title='Church Locations'
+            onPress={() => navigation.navigate('Events')}
+          />
+          <Cell
+            title='Contact'
+            onPress={() => navigation.navigate('Events')}
+          />
+        </TableView>
+      </ScrollView>
+    </SafeAreaView>
+  </BackgroundView>
 )
 
 More.navigationOptions = {
