2ac63f154d60211e553e3ae32abe6a632b00111a
Caleb Panza <caleb.panza@icloud.org>
2020-01-13 16:31:19 -0500

    Removed unecessary profile_old directory

--- src/profile__old/index.js
+++
@@ -1,8 +0,0 @@
-export CurrentUserProfile from './CurrentUserProfile'
-
-export AboutMe from './AboutMe'
-export AppInformation from './AppInformation'
-export CommunicationPreferences from './CommunicationPreferences'
-export FaithMilestones from './FaithMilestones'
-export FamilyInformation from './FamilyInformation'
-export UserSettings from './UserSettings'
