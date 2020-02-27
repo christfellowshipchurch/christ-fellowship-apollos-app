8d25fbc8710c804e7a823516e6a44ef32358a2fe
Caleb Panza <caleb.panza@icloud.org>
2020-02-27 11:43:53 -0500

    Dark Mode support

--- src/Providers.js
+++ src/Providers.js
@@ -7,35 +7,43 @@ import { NotificationsProvider } from '@apollosproject/ui-notifications';
 import { LiveProvider } from '@apollosproject/ui-connected';
 import { AuthProvider } from '@apollosproject/ui-auth';
 
+import { DynamicValue, useDynamicValue } from 'react-native-dark-mode';
+
 import NavigationService from './NavigationService';
 import ClientProvider from './client';
 import customTheme, { customIcons } from './theme';
 
-const AppProviders = (props) => (
-    <ClientProvider {...props}>
-        <NotificationsProvider
-            oneSignalKey={ApollosConfig.ONE_SIGNAL_KEY}
-            navigate={NavigationService.navigate}
-        >
-            <AuthProvider
-                navigateToAuth={() => NavigationService.navigate('Auth')}
+const dynamicTheme = new DynamicValue('light', 'dark');
+
+const AppProviders = (props) => {
+    const theme = useDynamicValue(dynamicTheme);
+
+    return (
+        <ClientProvider {...props}>
+            <NotificationsProvider
+                oneSignalKey={ApollosConfig.ONE_SIGNAL_KEY}
                 navigate={NavigationService.navigate}
-                closeAuth={() => NavigationService.navigate('Onboarding')}
             >
-                <MediaPlayerProvider>
-                    <AnalyticsProvider>
-                        <LiveProvider>
-                            <Providers
-                                themeInput={customTheme}
-                                iconInput={customIcons}
-                                {...props}
-                            />
-                        </LiveProvider>
-                    </AnalyticsProvider>
-                </MediaPlayerProvider>
-            </AuthProvider>
-        </NotificationsProvider>
-    </ClientProvider>
-);
+                <AuthProvider
+                    navigateToAuth={() => NavigationService.navigate('Auth')}
+                    navigate={NavigationService.navigate}
+                    closeAuth={() => NavigationService.navigate('Onboarding')}
+                >
+                    <MediaPlayerProvider>
+                        <AnalyticsProvider>
+                            <LiveProvider>
+                                <Providers
+                                    themeInput={{ ...customTheme, type: theme }}
+                                    iconInput={customIcons}
+                                    {...props}
+                                />
+                            </LiveProvider>
+                        </AnalyticsProvider>
+                    </MediaPlayerProvider>
+                </AuthProvider>
+            </NotificationsProvider>
+        </ClientProvider>
+    );
+};
 
 export default AppProviders;
