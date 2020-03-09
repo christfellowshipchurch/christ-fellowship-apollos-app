5280b6c8b5bc1f9cc8ffe363c9fc950a989bca00
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2020-03-09 15:33:00 -0400

    Cleaned up unecessary directories and fixed image sizes for On Boarding (#53)
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>

--- src/app-information/index.js
+++
@@ -1,3 +0,0 @@
-export PrivacyPolicy from './PrivacyPolicy';
-export TermsOfUse from './TermsOfUse';
-export ValueProp from './ValueProp';
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -6,7 +6,6 @@
 
 function loadStories() {
   require('../src/content-single/DevotionalContentItem/Devotional.stories');
-  require('../src/login/Onboarding.stories');
   require('../src/tabs/browse/FilterRow/FilterRow.stories');
   require('../src/ui/Cards/Cards.stories');
   require('../src/ui/LandingScreen/LandingScreen.stories');
@@ -14,12 +13,11 @@ function loadStories() {
   require('../src/ui/UserAvatarHeader__/UserAvatarView/UserAvatarView.stories');
   require('../src/ui/UserAvatarView__/UserAvatarView.stories');
   require('../src/ui/inputs/Inputs.stories');
-  
+
 }
 
 const stories = [
   '../src/content-single/DevotionalContentItem/Devotional.stories',
-  '../src/login/Onboarding.stories',
   '../src/tabs/browse/FilterRow/FilterRow.stories',
   '../src/ui/Cards/Cards.stories',
   '../src/ui/LandingScreen/LandingScreen.stories',
@@ -27,7 +25,7 @@ const stories = [
   '../src/ui/UserAvatarHeader__/UserAvatarView/UserAvatarView.stories',
   '../src/ui/UserAvatarView__/UserAvatarView.stories',
   '../src/ui/inputs/Inputs.stories',
-  
+
 ];
 
 module.exports = {
