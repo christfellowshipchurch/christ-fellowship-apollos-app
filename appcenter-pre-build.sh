50a7ce9531363b3efa366d71a00b52a5e9d23784
Caleb Panza <caleb.panza@icloud.org>
2020-04-08 16:56:49 -0400

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -11,15 +11,12 @@ cd ..
 echo AppCenter pre build script is running
 
 # You can comment this out if your app doesn't have an existing build number.
-# VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
-IOS_VERSION_CODE=$((123 + APPCENTER_BUILD_ID))
-ANDROID_VERSION_CODE=$((1735 + APPCENTER_BUILD_ID))
+VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
 
-echo Using "$IOS_VERSION_CODE" as iOS build number
-echo Using "$ANDROID_VERSION_CODE" as Android build number
+echo Using "$VERSION_CODE" as build number
 
 echo $VERSION_CODE
-plutil -replace CFBundleVersion -string "$IOS_VERSION_CODE" $(pwd)/ios/apolloschurchapp/Info.plist
-sed -i "" 's/versionCode [^"]*/versionCode '$ANDROID_VERSION_CODE'/' $(pwd)/android/app/build.gradle
+plutil -replace CFBundleVersion -string "$VERSION_CODE" $(pwd)/ios/apolloschurchapp/Info.plist
+sed -i "" 's/versionCode [^"]*/versionCode '$VERSION_CODE'/' $(pwd)/android/app/build.gradle
 
 yarn generate-stories
