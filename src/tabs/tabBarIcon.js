0cd7c8ae0afc2ba3c3a8192da39438a2b3f34756
Caleb Panza <caleb.panza@icloud.org>
2020-04-10 16:17:58 -0400

    Updated spacing of text under tabbar icons

--- src/tabs/tabBarIcon.js
+++ src/tabs/tabBarIcon.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Icon } from '@apollosproject/ui-kit';
+import { Icon, styled } from '@apollosproject/ui-kit';
+
+const StyledIcon = styled(({ theme }) => ({
+  marginBottom: 5,
+}))(Icon);
 
 const tabBarIcon = (name) => {
   function TabBarIcon({ tintColor }) {
-    return <Icon name={name} fill={tintColor} size={24} />;
+    return <StyledIcon name={name} fill={tintColor} size={24} />;
   }
   TabBarIcon.propTypes = {
     tintColor: PropTypes.string,
