2813851d408e6219c2e3ca0aa703733c181c1c77
Caleb Panza <caleb.panza@icloud.org>
2020-04-14 17:26:56 -0400

    TableView updates

--- src/ui/TableView/Cell.js
+++ src/ui/TableView/Cell.js
@@ -1,52 +1,46 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import {
     FlexedView,
     Touchable,
-    H4,
+    H5,
     styled,
     withTheme,
-} from '@apollosproject/ui-kit'
+} from '@apollosproject/ui-kit';
 
 const PaddedRow = styled(({ theme }) => ({
     flexDirection: 'row',
     justifyContent: 'center',
-    paddingVertical: theme.sizing.baseUnit,
-    paddingHorizontal: theme.sizing.baseUnit * 1.5
-}))(FlexedView)
+    paddingVertical: theme.sizing.baseUnit * 0.75,
+    paddingHorizontal: theme.sizing.baseUnit,
+}))(FlexedView);
 
 const Title = styled(({ theme }) => ({
     flex: 4,
     fontWeight: 'normal',
-}))(H4)
+}))(H5);
 
 const Icon = withTheme(({ theme }) => ({
     color: theme.colors.darkSecondary,
     style: {
         marginRight: theme.sizing.baseUnit * 0.5,
-    }
-}))(FontAwesomeIcon)
+    },
+}))(FontAwesomeIcon);
 
 const Cell = ({ title, icon, onPress }) => (
-    <Touchable
-        onPress={onPress}
-    >
+    <Touchable onPress={onPress}>
         <PaddedRow>
-            {!!icon && icon !== '' &&
-                <Icon icon={['fal', icon]} size={24} />
-            }
-            <Title>
-                {title}
-            </Title>
+            {!!icon && icon !== '' && <Icon icon={['fal', icon]} size={16} />}
+            <Title>{title}</Title>
             <Icon icon={['fal', 'angle-right']} size={24} />
         </PaddedRow>
     </Touchable>
-)
+);
 
 Cell.defaultProps = {
     icon: null,
     title: 'Cell Title',
-    onPress: () => { }
-}
+    onPress: () => { },
+};
 
-export default Cell
+export default Cell;
