3e491be2b29143cd07a736821bc75e319e4efabc
Caleb Panza <caleb.panza@icloud.org>
2020-04-14 17:27:24 -0400

    Added custom icons

--- src/theme/icons/index.js
+++ src/theme/icons/index.js
@@ -1,5 +1,8 @@
 /* Export your custom icon components here. */
-export Home from './home';
-export Calendar from './calendar';
-export Search from './search';
-export Bars from './bars';
+export Home from './Home';
+export Calendar from './Calendar';
+export Search from './Search';
+export Bars from './Bars';
+export EnvelopeOpenDollar from './EnvelopeOpenDollar';
+export Users from './Users';
+export Handshake from './Handshake';
