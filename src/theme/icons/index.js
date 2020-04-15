bcbb786d0f9054844184b3006fe9458a8ad7d0cc
Caleb Panza <caleb.panza@icloud.org>
2020-04-15 11:01:58 -0400

    Fix for icon naming

--- src/theme/icons/index.js
+++ src/theme/icons/index.js
@@ -2,14 +2,14 @@
  * Export your custom icon components here. 
  * For organization, please keep exports in alphabetical order
  */
-// export AngleRight from './AngleRight';
+export AngleRight from './AngleRight';
 export Bars from './Bars';
 export Calendar from './Calendar';
 export EnvelopeOpenDollar from './EnvelopeOpenDollar';
 export Handshake from './Handshake';
 export Home from './Home';
-// export List from './List';
-// export Lock from './Lock';
+export List from './List';
+export Lock from './Lock';
 export Search from './Search';
-// export ShareSquare from './ShareSquare';
+export ShareSquare from './ShareSquare';
 export Users from './Users';
