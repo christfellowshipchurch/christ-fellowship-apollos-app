db7ac66afb3be5abfa3b0d928b1f20bbd7072abe
Caleb Panza <caleb.panza@icloud.org>
2020-04-16 16:57:54 -0400

    Fixed Android crash error

---
+++ src/ui/Cards/HighlightCard/CardContentWrapper.android.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardContentWrapper = ({ children }) => <View>{children}</View>;
+
+CardContentWrapper.propTypes = {};
+
+CardContentWrapper.displayName = 'CardContentWrapper';
+
+export default CardContentWrapper;
