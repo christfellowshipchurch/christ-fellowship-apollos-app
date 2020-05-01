ed50a7d7a18d51de67dee7ee2bc7521506971dd4
Caleb Panza <caleb.panza@icloud.org>
2020-05-01 17:07:34 -0400

    Update Event dates

--- src/content-single/EventDateTimes/index.js
+++ src/content-single/EventDateTimes/index.js
@@ -1,198 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { Picker as NativePicker, View } from 'react-native';
-import {
-  get,
-  identity,
-  uniq,
-  flatMapDepth,
-  groupBy,
-  keys,
-  indexOf,
-} from 'lodash';
+import React from 'react';
+import { View } from 'react-native';
 import PropTypes from 'prop-types';
-import { DynamicValue, useDynamicValue } from 'react-native-dark-mode';
 import moment from 'moment';
 
-import {
-  FlexedView,
-  H4,
-  H6,
-  BodyText,
-  Touchable,
-} from '@apollosproject/ui-kit';
-import PickerList from '@apollosproject/ui-kit/src/inputs/Picker/PickerList';
+import { BodyText, Icon, styled, withTheme } from '@apollosproject/ui-kit';
 
-import {
-  HorizontalLine,
-  TextIconRow,
-  ScheduleContainer,
-  ContentContainer,
-} from './components';
-import { CURRENT_USER_CAMPUS } from './queries';
+const Row = styled(({ theme, fontWeight }) => ({
+  marginVertical: theme.sizing.baseUnit * 0.5,
+}))(View);
 
-const dynamicPickerColorValue = new DynamicValue('black', 'white');
+const StyledText = styled(({ theme, fontWeight }) => ({
+  fontSize: 20,
+  fontWeight,
+  marginLeft: theme.sizing.baseUnit * 0.5,
+}))(BodyText);
 
-const EventTimes = ({ date, times }) => {
-  const mDate = moment(date);
-  const currentUtcOffset = moment().format('ZZ');
+const StyledIcon = withTheme(({ theme }) => ({
+  size: 20,
+  fill: theme.colors.text.tertiary,
+}))(Icon);
+
+const TextIconRow = ({ icon, fontWeight, children }) => (
+  <View
+    style={{
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    }}
+  >
+    <StyledIcon name={icon} />
+    <StyledText style={{ fontWeight }}>{children}</StyledText>
+  </View>
+);
+
+const DateTime = ({ start }) => {
+  const mDate = moment(start);
 
   return (
-    <ScheduleContainer>
-      <TextIconRow icon="calendar-alt" fontSize={20} fontWeight="bold">
+    <Row>
+      <TextIconRow icon="calendar" fontSize={20} fontWeight="bold">
         {mDate.format('ddd MMM D')}
       </TextIconRow>
-      {times
+      <TextIconRow icon="clock" fontSize={20}>
+        {mDate.format('LT')}
+      </TextIconRow>
+    </Row>
+  );
+};
+
+const EventDateTimes = ({ events, loading }) => {
+  if (loading || events.length === 0) return null;
+
+  return (
+    <Row>
+      {events
         .sort((a, b) => moment(a.start).diff(moment(b.start)))
-        .map((t, i) => {
-          const utc = moment.utc(t.start);
-          const local = moment(utc).utcOffset(currentUtcOffset);
-
-          return (
-            <TextIconRow key={`EventTimesTime:${i}`} icon="clock" fontSize={20}>
-              {local.format('LT')}
-            </TextIconRow>
-          );
-        })}
-    </ScheduleContainer>
+        .map((event, i) => (
+          <DateTime key={`EventDateTime:${i}`} {...event} />
+        ))}
+    </Row>
   );
 };
 
-const CampusSelection = ({ loading, campuses, onChange, defaultCampus }) => {
-  const [focused, setFocused] = useState(false);
-  const selectLocation = 'Select Location';
-  const options = [selectLocation, ...campuses];
-  const [selected, setSelected] = useState(
-    options.includes(defaultCampus) ? defaultCampus : options[0]
-  );
-  const dynamicPickerItemStyle = {
-    color: useDynamicValue(dynamicPickerColorValue),
-  };
-
-  // when the selection changes, call the onChange method
-  useEffect(() => onChange(selected), [selected]);
-
-  if (!loading && campuses.length === 0)
-    return (
-      <BodyText style={{ fontStyle: 'italic' }}>
-        Check back soon for dates and times
-      </BodyText>
-    );
-
-  return (
-    <Touchable onPress={() => setFocused(!focused)}>
-      <View>
-        <H4 isLoading={loading}>{selected}</H4>
-
-        {!loading && <H6>{selected !== selectLocation && 'Change'}</H6>}
-
-        <PickerList
-          selectedValue={indexOf(options, selected)}
-          // Sets the value of the Picker locally,
-          //    but without triggering the callback func
-          onValueChange={(key) => {
-            const index = parseInt(key, 10);
-            setSelected(options[index]);
-          }}
-          // When the user manually presses the DONE button,
-          //    we fire the callback function with the new value
-          onRequestClose={() => {
-            setFocused(false);
-            onChange(selected);
-          }}
-          focused={focused}
-          itemStyle={dynamicPickerItemStyle}
-        >
-          {options.map((o, i) => (
-            <NativePicker.Item key={i} label={o} value={i} />
-          ))}
-        </PickerList>
-      </View>
-    </Touchable>
-  );
-};
-
-const EventOcurrences = ({ defaultCampus, events, loading }) => {
-  const [visibleOccurrences, setVisibleOccurrences] = useState([]);
-  const campusOptions = uniq(
-    flatMapDepth(events.map((e) => e.campuses.map((c) => c.name)), identity, 2)
-  );
-  const groupByLocations = groupBy(visibleOccurrences, 'location');
-  const groupByLocationDate = keys(groupByLocations).map((l) => {
-    const dateTimes = groupBy(groupByLocations[l], (o) =>
-      moment(o.start).format('YYYY-MM-DD')
-    );
-
-    return { location: l, dateTimes };
-  });
-
-  const onChange = (campus) => {
-    const campusEvents = events.filter((e) =>
-      e.campuses.find((c) => c.name === campus)
-    );
-
-    setVisibleOccurrences(campusEvents);
-  };
-
-  if (loading) return null;
-
-  return (
-    <ContentContainer>
-      <CampusSelection
-        campuses={campusOptions}
-        loading={loading}
-        onChange={onChange}
-        defaultCampus={defaultCampus}
-      />
-
-      {groupByLocationDate.map((event, i) => {
-        const { location, dateTimes } = event;
-        return (
-          <FlexedView
-            key={`Event:${location}${i}`}
-            style={{ flexDirection: 'column' }}
-          >
-            {keys(dateTimes).map((date) => (
-              <EventTimes
-                key={`EventOccurenceDate:${date}`}
-                date={date}
-                times={dateTimes[date]}
-              />
-            ))}
-
-            <HorizontalLine />
-          </FlexedView>
-        );
-      })}
-    </ContentContainer>
-  );
-};
-
-EventOcurrences.propTypes = {
-  events: PropTypes.array,
-  defaultCampus: PropTypes.string,
+EventDateTimes.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.object),
   loading: PropTypes.bool,
 };
 
-EventOcurrences.defaultProps = {
+EventDateTimes.defaultProps = {
   events: [],
+  loading: false,
 };
 
-const WithDefaultCampus = ({ events }) => {
-  const { loading, error, data } = useQuery(CURRENT_USER_CAMPUS);
-  const currentUserCampus = get(data, 'currentUser.profile.campus.name', '');
-
-  return (
-    <EventOcurrences
-      events={events}
-      defaultCampus={currentUserCampus}
-      loading={loading}
-    />
-  );
-};
-
-WithDefaultCampus.propTypes = {
-  events: PropTypes.array,
-};
-
-export default WithDefaultCampus;
+export default EventDateTimes;
