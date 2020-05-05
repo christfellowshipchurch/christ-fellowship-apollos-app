27621e2039f348a5e6414988c965be9fc3080e98
Caleb Panza <caleb.panza@icloud.org>
2020-05-05 12:22:17 -0400

    Bugsnag configuration and package

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import SplashScreen from 'react-native-splash-screen';
 import { useDarkModeContext } from 'react-native-dark-mode';
 
+import ApollosConfig from '@apollosproject/config';
 import { BackgroundView, ThemeMixin } from '@apollosproject/ui-kit';
 import Passes from '@apollosproject/ui-passes';
 import { MapViewConnected as Location } from '@apollosproject/ui-mapview';
 import { MediaPlayer } from '@apollosproject/ui-media-player';
 import { ProtectedRoute } from '@apollosproject/ui-auth';
+import { Client } from 'bugsnag-react-native';
 import Auth from './auth';
 import StatusBar from './ui/StatusBar';
 
@@ -22,6 +24,9 @@ import EditUser from './edit-user';
 import LandingScreen from './LandingScreen';
 import Onboarding from './ui/Onboarding';
 import AuthBackground from './ui/AuthBackground';
+
+// bugsnag configuration
+const bugsnag = new Client(ApollosConfig.BUGSNAG_KEY);
 
 // Hack to avoid needing to pass emailRequired through the navigator.navigate
 const EnhancedAuth = (props) => (
