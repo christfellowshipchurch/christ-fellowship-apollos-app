5e59539baa86bead1fee6bae86043a3a9db50fe2
Caleb Panza <caleb.panza@icloud.org>
2020-05-19 17:51:31 -0400

    Deprecated Rock Authed browser for new link router

--- src/web-browser/index.js
+++ src/web-browser/index.js
@@ -1,27 +1,15 @@
 import React from 'react';
+import { withApollo } from 'react-apollo';
 import { RockAuthedWebBrowser } from '@apollosproject/ui-connected';
-import { openLink as openLinkInExternalBrowser } from '../utils/linking';
-
-// TODO : more mature way of handling url parsing
-const RESTRICTED_HOST_NAMES = ['pushpay.com'];
-const RESTRICTED_QUERY_PARAMS = ['mobileApp=external'];
+import gql from 'graphql-tag';
+import NavigationService from '../NavigationService';
+import { routeLink } from '../utils/linking';
 
 const Browser = ({ children }) => (
   <RockAuthedWebBrowser>
     {(openLinkInternalBrowser) =>
       children((url) => {
-        const restrictedHostName = RESTRICTED_HOST_NAMES.find((host) =>
-          url.includes(host)
-        );
-        const restrictedQueryParam = RESTRICTED_QUERY_PARAMS.find((param) =>
-          url.includes(param)
-        );
-
-        if (restrictedHostName || restrictedQueryParam) {
-          openLinkInExternalBrowser({ uri: url });
-        } else {
-          openLinkInternalBrowser(url);
-        }
+        routeLink(url);
       })
     }
   </RockAuthedWebBrowser>
