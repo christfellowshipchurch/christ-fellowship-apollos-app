c1abd0ad1ade834cf5350ae6ec553ff29e313915
Caleb Panza <caleb.panza@icloud.org>
2020-06-03 17:33:11 -0400

    Search styles

--- src/tabs/discover/SearchFeed/index.js
+++ src/tabs/discover/SearchFeed/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { View } from 'react-native';
 import { withProps } from 'recompose';
 import { withNavigation } from 'react-navigation';
 import { Query } from 'react-apollo';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
 
-import { FeedView } from '@apollosproject/ui-kit';
-import { SearchCardConnected } from '@apollosproject/ui-connected';
+import { FeedView, styled } from '@apollosproject/ui-kit';
 
+import ContentCardConnected from '../../../ui/ContentCardConnected';
+import ActionRow from '../../../ui/ActionRow';
 import GET_SEARCH_RESULTS from './getSearchResults';
 import NoResults from './NoResults';
+
+const SearchCardConnected = (props) => (
+  <ContentCardConnected {...props} card={ActionRow} />
+);
 
 // this could be refactored into a custom effect hook 💥
 const StyledFeedView = withProps(({ hasContent }) => ({
@@ -18,15 +24,22 @@ const StyledFeedView = withProps(({ hasContent }) => ({
   },
 }))(FeedView);
 
+// Hack to get around a weird issue where the tabbar
+// is cutting off the last row of cards
+const EndCapSpacer = styled(({ theme }) => ({
+  height: 150,
+}))(View);
+
 const handleOnPress = ({ navigation, item }) => {
-  const id = get(item, 'node.id', null);
+  const id = get(item, 'id', null);
   return navigation.navigate('ContentSingle', {
     itemId: id,
     transitionKey: item.transitionKey,
   });
 };
 
-const keyExtractor = (item) => item && get(item, 'node.id', null);
+const keyExtractor = (item) => item && get(item, 'id', null);
+const mapData = (data) => get(data, 'search.edges', []).map(({ node }) => node);
 
 const SearchFeed = withNavigation(({ navigation, searchText }) => (
   <Query
@@ -37,14 +50,16 @@ const SearchFeed = withNavigation(({ navigation, searchText }) => (
     {({ loading, error, data, refetch }) => (
       <StyledFeedView
         ListItemComponent={SearchCardConnected}
-        content={get(data, 'search.edges', [])}
+        content={mapData(data)}
         ListEmptyComponent={() => <NoResults searchText={searchText} />}
+        ListFooterComponent={<EndCapSpacer />}
         hasContent={get(data, 'search.edges', []).length}
         isLoading={loading}
         error={error}
         refetch={refetch}
         onPressItem={(item) => handleOnPress({ navigation, item })}
         keyExtractor={keyExtractor}
+        numColumns={1}
       />
     )}
   </Query>
--- src/ui/SearchInputHeader/index.js
+++ src/ui/SearchInputHeader/index.js
@@ -6,26 +6,7 @@ import { styled, PaddedView, SearchInput } from '@apollosproject/ui-kit';
 
 const HeaderBorder = styled(
     ({ theme }) => ({
-        paddingBottom: theme.sizing.baseUnit / 2,
-        /* It's unclear why this is necessary but without it the layout breaks on both platforms. Limited
-         * research suggest that without a background color the shadows don't know what to blend with so
-         * the view collapses. */
-        backgroundColor: theme.colors.background.paper,
-        // Renders the same shadows that React Navigation does.
-        ...Platform.select({
-            ios: {
-                shadowColor: 'rgba(0, 0, 0, 0.3)',
-                shadowOpacity: 0.85,
-                shadowRadius: 0,
-                shadowOffset: {
-                    width: 0,
-                    height: StyleSheet.hairlineWidth,
-                },
-            },
-            android: {
-                elevation: 4,
-            },
-        }),
+        paddingLeft: theme.sizing.baseUnit,
     }),
     'SearchInputHeader.HeaderBorder'
 )(PaddedView);
