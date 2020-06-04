b5d3dbdf592130b131f722ab642a20825a23aaa8
Caleb Panza <caleb.panza@icloud.org>
2020-06-04 10:13:35 -0400

    Media Query for large devices

--- src/tabs/discover/SearchFeed/index.js
+++ src/tabs/discover/SearchFeed/index.js
@@ -6,23 +6,38 @@ import { Query } from 'react-apollo';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
 
-import { FeedView, styled } from '@apollosproject/ui-kit';
+import {
+  FeedView,
+  styled,
+  withMediaQuery,
+  FlexedView,
+} from '@apollosproject/ui-kit';
 
 import ContentCardConnected from '../../../ui/ContentCardConnected';
 import ActionRow from '../../../ui/ActionRow';
 import GET_SEARCH_RESULTS from './getSearchResults';
 import NoResults from './NoResults';
 
-const SearchCardConnected = (props) => (
-  <ContentCardConnected {...props} card={ActionRow} />
-);
+const SearchCardConnected = withProps(() => ({
+  card: ActionRow,
+}))(ContentCardConnected);
 
 // this could be refactored into a custom effect hook 💥
-const StyledFeedView = withProps(({ hasContent }) => ({
-  contentContainerStyle: {
-    ...(hasContent ? {} : { flex: 1 }),
-  },
-}))(FeedView);
+const StyledFeedView = withMediaQuery(
+  ({ md }) => ({ maxWidth: md }),
+  withProps(({ hasContent }) => ({
+    numColumns: 1,
+    contentContainerStyle: {
+      ...(hasContent ? {} : { flex: 1 }),
+    },
+  })),
+  withProps(({ hasContent }) => ({
+    numColumns: 2,
+    contentContainerStyle: {
+      ...(hasContent ? {} : { flex: 1 }),
+    },
+  }))
+)(FeedView);
 
 // Hack to get around a weird issue where the tabbar
 // is cutting off the last row of cards
@@ -40,6 +55,11 @@ const handleOnPress = ({ navigation, item }) => {
 
 const keyExtractor = (item) => item && get(item, 'id', null);
 const mapData = (data) => get(data, 'search.edges', []).map(({ node }) => node);
+const renderItem = ({ item }) => (
+  <FlexedView>
+    <SearchCardConnected contentId={item.id} {...item} />
+  </FlexedView>
+);
 
 const SearchFeed = withNavigation(({ navigation, searchText }) => (
   <Query
@@ -49,7 +69,7 @@ const SearchFeed = withNavigation(({ navigation, searchText }) => (
   >
     {({ loading, error, data, refetch }) => (
       <StyledFeedView
-        ListItemComponent={SearchCardConnected}
+        renderItem={renderItem}
         content={mapData(data)}
         ListEmptyComponent={() => <NoResults searchText={searchText} />}
         ListFooterComponent={<EndCapSpacer />}
@@ -59,7 +79,6 @@ const SearchFeed = withNavigation(({ navigation, searchText }) => (
         refetch={refetch}
         onPressItem={(item) => handleOnPress({ navigation, item })}
         keyExtractor={keyExtractor}
-        numColumns={1}
       />
     )}
   </Query>
