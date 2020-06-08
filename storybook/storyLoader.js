d440b8a8deeec2b74745b76bfe7d53ab465a2b52
Caleb Panza <caleb.panza@icloud.org>
2020-06-08 17:15:08 -0400

    CardFeed stories

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -7,6 +7,7 @@
 function loadStories() {
   require('../src/content-single/DevotionalContentItem/Devotional.stories');
   require('../src/ui/ActionBar/ActionBar.stories');
+  require('../src/ui/CardFeeds/CardFeed.stories');
   require('../src/ui/Cards/Cards.stories');
   require('../src/ui/LandingScreen/LandingScreen.stories');
   require('../src/ui/Onboarding/Onboarding.stories');
@@ -17,6 +18,7 @@ function loadStories() {
 const stories = [
   '../src/content-single/DevotionalContentItem/Devotional.stories',
   '../src/ui/ActionBar/ActionBar.stories',
+  '../src/ui/CardFeeds/CardFeed.stories',
   '../src/ui/Cards/Cards.stories',
   '../src/ui/LandingScreen/LandingScreen.stories',
   '../src/ui/Onboarding/Onboarding.stories',
