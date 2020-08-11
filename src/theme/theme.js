600f46a4e40321d06961cb39da966324abd855b8
Caleb Panza <caleb.panza@icloud.org>
2020-08-11 11:55:26 -0400

    Moving overrides to their own directory for readability

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,19 +1,10 @@
-import React from 'react'
 import { Platform } from 'react-native';
 import Color from 'color';
 import dark from './dark-theme';
 import onboarding from './onboarding';
 import authEntry from './authEntry';
-import { Text } from 'react-native';
 
-import { ScriptureText, VerseNumber } from '@apollosproject/ui-scripture';
-import { BodyText } from '@apollosproject/ui-kit';
-import { 
-    AddPrayerScreenConnected,
-    PrayerDialogScreen,
-    ConfirmationDialogScreen,
- } from '@apollosproject/ui-prayer/src/screens'
- import { PrayerCard } from '@apollosproject/ui-prayer'
+import { UIPrayerOverrides } from './overrides';
 
 /* Add your custom theme definitions below. Anything that is supported in UI-Kit Theme can be
  overridden and/or customized here! */
@@ -200,54 +191,7 @@ const overrides = ({ colors }) => ({
     'ui-auth.FieldLabel': {
         color: '#FCFCFC',
     },
-    'ui-prayer.PrayerExperienceConnected': {
-        showOnboarding: true,
-    },
-    'ui-prayer.PrayerInput.PromptIcon': {
-        name: 'plus',
-    },
-    // 'ui-prayer.PrayerInput.Prompt': {
-        
-    // },
-    'ui-prayer.PrayerExperience': {
-        AddPrayerComponent: (props) => (
-            <AddPrayerScreenConnected 
-                {...props}
-                title="How can others pray for you?" 
-                skipText='Pray for others'
-                primaryButtonText='Share my prayer'
-                AddedPrayerComponent={(props) => (
-                    <ConfirmationDialogScreen
-                        {...props}
-                        body={
-                            <Text>
-                              <ScriptureText>
-                                This is the confidence we have in approaching God: that if we ask
-                                anything according to his will, he hears us.{'\n'}
-                                <VerseNumber>1 John 5:14</VerseNumber>
-                              </ScriptureText>
-                              {'\n\n'}
-                              <BodyText>We’re partnering with you in prayer today!</BodyText>
-                            </Text>
-                          }
-                    />
-                )}
-                PrayerCardComponent={(props) => (
-                    <PrayerCard
-                        {...props}
-                        // prayer="I'd like prayer for..."
-                    />
-                )}
-            />
-        ),
-        OnboardingComponent: (props) => (
-            <PrayerDialogScreen 
-                {...props}
-                title='Let’s pray today!'
-                body="As we always say, prayer is our first response, not our last resort. Let your church family know how they can be praying for you, and then pray for their requests too! Don't walk through life alone—join together in prayer!"
-            />
-        ),
-      },
+    ...UIPrayerOverrides,
 });
 
 const buttons = ({ colors: themeColors }) => ({
