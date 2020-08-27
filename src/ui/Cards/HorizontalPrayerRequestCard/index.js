0193dc19c51a2cfab244fdfbbbe2810c4b21f711
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2020-08-27 17:21:27 -0400

    [CFDP-878] My prayers (#121)
    * [CFDP-877] profile updates
    
    * Fixed a random issue that's been bugging me
    
    On EditProfile, the entire screen is now scrollable and will avoid the keyboard when interacting with fields
    
    * [CFDP-878]
    
    * Creates a new screen for viewing a list of my prayers
    * Creates a new screen for viewing a single prayer
    * Adds a preview of my 5 most recent prayers to the Profile tab
    
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>

---
+++ src/ui/Cards/HorizontalPrayerRequestCard/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+    Card,
+    CardContent,
+    H5,
+    styled,
+    withIsLoading,
+} from '@apollosproject/ui-kit';
+
+import DateLabel from '../../DateLabel';
+
+const CARD_WIDTH = 212;
+
+const CardWrapper = styled(({ customTheme, theme }) => ({
+    width: CARD_WIDTH,
+    height: 150,
+    flex: 1,
+}))(Card);
+
+const Content = styled(({ theme }) => ({
+    paddingHorizontal: theme.sizing.baseUnit,
+    paddingBottom: theme.sizing.baseUnit,
+}))(CardContent);
+
+const Text = styled(({ theme }) => ({
+    color: theme.colors.darkPrimary,
+}))(H5);
+
+const HorizontalPrayerRequestCard = withIsLoading(
+    ({ text, date, isLoading }) => (
+        <CardWrapper isLoading={isLoading} inHorizontalList>
+            <Content>
+                {date ? <DateLabel date={date} isLoading={isLoading} /> : null}
+                {text ? <Text isLoading={isLoading}>{text}</Text> : null}
+            </Content>
+        </CardWrapper>
+    )
+);
+
+HorizontalPrayerRequestCard.propTypes = {
+    text: PropTypes.string,
+    date: PropTypes.string,
+};
+
+HorizontalPrayerRequestCard.displayName = 'HorizontalPrayerRequestCard';
+HorizontalPrayerRequestCard.cardWidth = CARD_WIDTH;
+
+export default HorizontalPrayerRequestCard;
