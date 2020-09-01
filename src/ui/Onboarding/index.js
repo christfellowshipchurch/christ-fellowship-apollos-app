8e12882966a98ff450c77f93e2e2dbd11af504a2
Caleb Panza <caleb.panza@icloud.org>
2020-09-01 15:34:18 -0400

    Apollos upgrade tool comparison

--- src/ui/Onboarding/index.js
+++ src/ui/Onboarding/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Query } from "react-apollo";
 import {
   checkNotifications,
   openSettings,
   requestNotifications,
-  RESULTS,
-} from 'react-native-permissions';
+  RESULTS
+} from "react-native-permissions";
 
-import { withThemeMixin, NavigationService } from '@apollosproject/ui-kit';
+import { withThemeMixin, NavigationService } from "@apollosproject/ui-kit";
 import {
   AskNotificationsConnected,
   FeaturesConnected,
   LocationFinderConnected,
   OnboardingSwiper,
-} from '@apollosproject/ui-onboarding';
+  onboardingComplete,
+  WITH_USER_ID
+} from "@apollosproject/ui-onboarding";
 
-import AuthBackground from '../AuthBackground';
+import AuthBackground from "../AuthBackground";
 
 const { resetAction } = NavigationService;
 
@@ -31,36 +34,43 @@ function Onboarding({ navigation }) {
             <LocationFinderConnected
               onPressPrimary={swipeForward}
               onNavigate={() => {
-                navigation.navigate('Location');
+                navigation.navigate("Location");
               }}
-              primaryNavText={'Next'}
+              primaryNavText={"Next"}
               description="We'll use your location to connect you with your nearby campus and community."
             />
-            <AskNotificationsConnected
-              onRequestPushPermissions={(update) => {
-                checkNotifications().then((checkRes) => {
-                  if (checkRes.status === RESULTS.DENIED) {
-                    requestNotifications(['alert', 'badge', 'sound']).then(
-                      () => {
-                        update();
-                      }
-                    );
-                  } else {
-                    openSettings();
-                  }
-                });
-              }}
-              onPressPrimary={() =>
-                navigation.dispatch(
-                  resetAction({
-                    navigatorName: 'Tabs',
-                    routeName: 'Home',
-                  })
-                )
-              }
-              primaryNavText={'Finish'}
-              description="We'll let you know when important things are happening and keep you in the loop."
-            />
+            <Query query={WITH_USER_ID} fetchPolicy="network-only">
+              {({
+                data: { currentUser: { id } = { currentUser: { id: null } } }
+              }) => (
+                  <AskNotificationsConnected
+                    onPressPrimary={() => {
+                      onboardingComplete({ userId: id });
+                      navigation.dispatch(
+                        NavigationService.resetAction({
+                          navigatorName: "Tabs",
+                          routeName: "Home"
+                        })
+                      );
+                    }}
+                    onRequestPushPermissions={update => {
+                      checkNotifications().then(checkRes => {
+                        if (checkRes.status === RESULTS.DENIED) {
+                          requestNotifications(["alert", "badge", "sound"]).then(
+                            () => {
+                              update();
+                            }
+                          );
+                        } else {
+                          openSettings();
+                        }
+                      });
+                    }}
+                    primaryNavText={"Finish"}
+                    description="We'll let you know when important things are happening and keep you in the loop."
+                  />
+                )}
+            </Query>
           </>
         )}
       </OnboardingSwiper>
@@ -69,18 +79,18 @@ function Onboarding({ navigation }) {
 }
 
 const OnboardingWithTheme = withThemeMixin({
-  type: 'onboarding',
+  type: "onboarding",
   colors: {
     background: {
-      paper: 'white',
-    },
-  },
+      paper: "white"
+    }
+  }
 })(Onboarding);
 
 OnboardingWithTheme.navigationOptions = {
-  title: 'Onboarding',
+  title: "Onboarding",
   header: null,
-  gesturesEnabled: false,
+  gesturesEnabled: false
 };
 
 export default OnboardingWithTheme;
