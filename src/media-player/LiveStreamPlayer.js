7d82a4e4a86b0054bfe17f0bfb908954f6db74bf
gerard sychay <hellogerard@gmail.com>
2020-10-01 23:22:15 -0400

    Fix MessagesBanner on Android
    - Idk... flexbox doesn't work on TouchableNativeFeedback?

--- src/media-player/LiveStreamPlayer.js
+++ src/media-player/LiveStreamPlayer.js
@@ -9,6 +9,7 @@ import {
   Platform,
   StatusBar,
   SafeAreaView,
+  TouchableOpacity,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { Query, withApollo } from 'react-apollo';
@@ -20,7 +21,6 @@ import {
   H5,
   Icon,
   NavigationService,
-  Touchable,
   LayoutConsumer,
 } from '@apollosproject/ui-kit';
 
@@ -55,7 +55,7 @@ const MessagesBanner = styled(({ theme }) => ({
   justifyContent: 'space-between',
   alignItems: 'center',
   paddingHorizontal: theme.sizing.baseUnit,
-}))(Touchable);
+}))(TouchableOpacity);
 
 const MessagesBannerText = styled(({ theme }) => ({
   color: theme.colors.background.paper,
@@ -331,6 +331,7 @@ class LiveStreamPlayer extends PureComponent {
                 : {})}
             >
               <MessagesBanner
+                activeOpacity={0.5}
                 onPress={() => {
                   this.props.client.mutate({ mutation: EXIT_FULLSCREEN });
                   setTimeout(() => {
@@ -426,7 +427,10 @@ class LiveStreamPlayer extends PureComponent {
                 ...StyleSheet.absoluteFillObject,
                 top: this.bannerHeight.interpolate({
                   inputRange: [0, 1],
-                  outputRange: [LIVESTREAM_HEIGHT, LIVESTREAM_HEIGHT + notch + BANNER_HEIGHT],
+                  outputRange: [
+                    LIVESTREAM_HEIGHT,
+                    LIVESTREAM_HEIGHT + notch + BANNER_HEIGHT,
+                  ],
                 }),
               }}
             >
