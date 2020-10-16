e80f08a99a5f91e8656a5b5deb6baad4207a9e41
Ryan Davidson <ryan@differential.com>
2020-10-16 13:17:15 -0400

    Add feature flag check to group chat button

--- src/group-single/GroupChatButton.js
+++ src/group-single/GroupChatButton.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { styled, Button, NavigationService } from '@apollosproject/ui-kit';
 
+import { useFeatureFlag } from '../hooks';
+
 const StyledButton = styled(({ theme }) => ({
   marginBottom: theme.sizing.baseUnit,
 }))(Button);
 
 const GroupChatButton = ({ channelId, groupName }) => {
-  if (!channelId) {
+  const { enabled } = useFeatureFlag({ key: 'GROUP_CHAT' });
+
+  if (!enabled || !channelId) {
     return null;
   }
 
