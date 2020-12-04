2caacb5503db5cce9370ed9851c7b4fadf5cc06f
Jordan Wade <jjwade31@gmail.com>
2020-12-04 14:20:49 -0600

    Remove unused imports

--- src/ui/Cards/GroupCard/index.js
+++ src/ui/Cards/GroupCard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
-import { get, head, drop } from 'lodash';
+import { get } from 'lodash';
 
 import {
   Card,
