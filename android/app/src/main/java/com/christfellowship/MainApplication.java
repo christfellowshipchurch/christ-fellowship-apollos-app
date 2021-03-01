0bccdad52ae51b1c5367ed9beccca38edeb60c73
Caleb Panza <caleb.panza@icloud.org>
2021-03-01 13:04:22 -0500

    Android configuration

--- android/app/src/main/java/com/christfellowship/MainApplication.java
+++ android/app/src/main/java/com/christfellowship/MainApplication.java
@@ -1,18 +1,16 @@
 package com.christfellowship;
 
 import android.app.Application;
-import android.util.Log;
+import android.content.Context;
 
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.ReactInstanceManager;
+import java.lang.reflect.InvocationTargetException;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import org.wonday.orientation.OrientationPackage;
-import org.wonday.orientation.OrientationActivityLifecycle;
 
 import java.util.Arrays;
 import java.util.List;
@@ -49,6 +47,38 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    registerActivityLifecycleCallbacks(OrientationActivityLifecycle.getInstance());
+    // ? if we run into issues, this could be part of the problem
+    // registerActivityLifecycleCallbacks(OrientationActivityLifecycle.getInstance());
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+  /**
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+   *
+   * @param context
+   * @param reactInstanceManager
+   */
+  private static void initializeFlipper(
+      Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.rndiffapp.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
