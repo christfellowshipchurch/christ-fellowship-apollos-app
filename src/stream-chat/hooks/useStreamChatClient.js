018b07c427cce5fd369b62c9fa538486f2d57c66
Caleb Panza <caleb.panza@icloud.org>
2021-04-15 16:13:22 -0400

    Removed the static user from android

--- src/stream-chat/hooks/useStreamChatClient.js
+++ src/stream-chat/hooks/useStreamChatClient.js
@@ -13,27 +13,13 @@
 import { useEffect, useState } from 'react';
 
 import ApollosConfig from '@apollosproject/config';
-import { StreamChat } from 'stream-chat';
 import { useCurrentUser } from 'hooks';
 import { StreamChatClient } from '../client';
 
 const API_KEY = ApollosConfig.STREAM_CHAT_API_KEY;
 
-const staticUser = {
-  id: 'AuthenticatedUser:64302734012a9ca790d3ef795bdcea48',
-  firstName: 'Caleb',
-  lastName: 'Panza',
-  photo: {
-    uri:
-      'https://cloudfront.christfellowship.church/GetImage.ashx?guid=3376aa0d-5610-4a8a-ae24-046250ebf297&mode=crop&h=150&w=150&format=jpg&quality=70',
-  },
-  streamChatToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjQzMDI3MzQwMTJhOWNhNzkwZDNlZjc5NWJkY2VhNDgifQ.NCfg-cpPixn2NNN9NOT4fj8gNLKF6METGQiBXHcM7rs',
-};
-
 export default () => {
-  // const { id, firstName, lastName, photo, streamChatToken } = useCurrentUser();
-  const { id, firstName, lastName, photo, streamChatToken } = staticUser;
+  const { id, firstName, lastName, photo, streamChatToken } = useCurrentUser();
   const [chatClient, setChatClient] = useState();
   const [isConnecting, setIsConnecting] = useState(true);
 
@@ -65,17 +51,20 @@ export default () => {
     chatClient?.disconnect();
   };
 
-  useEffect(() => {
-    if (streamChatToken) {
-      connectUser({
-        apiKey: API_KEY,
-        userId: id.split(':')[1],
-        userImage: photo?.uri,
-        userName: `${firstName} ${lastName}`,
-        userToken: streamChatToken,
-      });
-    }
-  }, []);
+  useEffect(
+    () => {
+      if (streamChatToken) {
+        connectUser({
+          apiKey: API_KEY,
+          userId: id.split(':')[1],
+          userImage: photo?.uri,
+          userName: `${firstName} ${lastName}`,
+          userToken: streamChatToken,
+        });
+      }
+    },
+    [streamChatToken]
+  );
 
   return {
     chatClient,
