24c6de4a26643d8b9d159ddc486228dfea26777a
Caleb Panza <caleb.panza@icloud.org>
2021-05-26 12:32:03 -0400

    Update ContentBlockFeature

--- src/features/FeaturesFeed/ContentBlockFeature/ContentBlockFeature.js
+++ src/features/FeaturesFeed/ContentBlockFeature/ContentBlockFeature.js
@@ -57,12 +57,17 @@ const Spacing = styled(({ theme }) => ({
 
 // :: Styles
 // :: ================================== ::
+const StyledH3 = styled(({ theme }) => ({
+  paddingBottom: theme.sizing.baseUnit,
+}))(H3);
+
 const StyledH5 = styled(({ theme }) => ({
   color: theme.colors.text.tertiary,
+  paddingBottom: theme.sizing.baseUnit * 0.5,
 }))(H5);
 
 const StyledButton = styled(({ theme }) => ({
-  marginVertical: theme.sizing.baseUnit * 0.5,
+  marginVertical: theme.sizing.baseUnit * 0.25,
 }))(Button);
 
 // :: Render Methods
@@ -80,7 +85,7 @@ const renderMedia = ({ mediaQuery, orientation, coverImage, videos }) => {
         }}
       >
         <InlineMediaPlayer
-          source={videos}
+          source={videos[0]?.sources[0]}
           coverImage={coverImage?.sources[0]}
         />
       </Spacing>
@@ -95,7 +100,7 @@ const ConditionalRender = ({ condition, children }) =>
 
 const ContentBlockFeature = ({
   title,
-  summary,
+  subtitle,
   htmlContent,
   coverImage,
   orientation,
@@ -105,9 +110,9 @@ const ContentBlockFeature = ({
   onPressItem,
 }) => {
   const showTitle = title && !isEmpty(title);
-  const showSummary = summary && !isEmpty(summary);
+  const showSubtitle = subtitle && !isEmpty(subtitle);
   const showContent = htmlContent && !isEmpty(htmlContent);
-  const showActions = !isEmpty(actions);
+  const showActions = !isEmpty(actions) && Array.isArray(actions);
 
   return (
     <Container
@@ -123,12 +128,12 @@ const ContentBlockFeature = ({
           mediaQuery,
         }}
       >
-        <ConditionalRender condition={showSummary}>
-          <StyledH5>{summary}</StyledH5>
+        <ConditionalRender condition={showTitle}>
+          <StyledH3>{title}</StyledH3>
         </ConditionalRender>
 
-        <ConditionalRender condition={showTitle}>
-          <H3>{title}</H3>
+        <ConditionalRender condition={showSubtitle}>
+          <StyledH5>{subtitle}</StyledH5>
         </ConditionalRender>
 
         <ConditionalRender condition={showContent}>
@@ -136,15 +141,16 @@ const ContentBlockFeature = ({
         </ConditionalRender>
 
         <ConditionalRender condition={showActions}>
-          {actions.map(({ title: actionTitle, action, relatedNode }, i) => (
-            <StyledButton
-              key={`${title}${relatedNode?.id}`}
-              title={actionTitle}
-              onPress={() => onPressItem({ action, relatedNode })}
-              bordered={i > 0}
-              pill={false}
-            />
-          ))}
+          {Array.isArray(actions) &&
+            actions.map(({ title: actionTitle, action, relatedNode }, i) => (
+              <StyledButton
+                key={`${title}${relatedNode?.id}`}
+                title={actionTitle}
+                onPress={() => onPressItem({ action, relatedNode })}
+                bordered={i > 0}
+                pill={false}
+              />
+            ))}
         </ConditionalRender>
       </Spacing>
     </Container>
@@ -153,7 +159,7 @@ const ContentBlockFeature = ({
 
 ContentBlockFeature.propTypes = {
   title: PropTypes.string,
-  summary: PropTypes.string,
+  subtitle: PropTypes.string,
   htmlContent: PropTypes.string,
   coverImage: ImageSourceType,
   videos: ImageSourceType,
